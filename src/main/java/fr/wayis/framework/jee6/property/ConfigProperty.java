6d58ea1b4a91128b740571ac2428956371913ab5
Luc LABBE <luc.labbe@inge.neo-soft.fr>
2014-03-06 21:32:54 +0100

    Add config property util class.

---
+++ src/main/java/fr/wayis/framework/jee6/property/ConfigProperty.java
@@ -0,0 +1,29 @@
+package fr.wayis.framework.jee6.property;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Qualifier for Configuration properties.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+public @interface ConfigProperty {
+
+    @Nonbinding
+    public String bundle() default "deploy";
+
+    @Nonbinding
+    public String key() default "";
+
+    @Nonbinding
+    public boolean mandatory() default false;
+
+    @Nonbinding
+    public String defaultValue() default "";
+}
---
+++ src/main/java/fr/wayis/framework/jee6/property/ConfigPropertyProducer.java
@@ -0,0 +1,62 @@
+package fr.wayis.framework.jee6.property;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * Producer associated to the {@link ConfigProperty} qualifier.
+ */
+public class ConfigPropertyProducer {
+
+    @Produces
+    @ConfigProperty
+    public String getStringProperty(final InjectionPoint p) {
+        return getPropertyValue(p);
+    }
+
+    @Produces
+    @ConfigProperty
+    public Integer getIntegerProperty(final InjectionPoint p) {
+        final String value = getPropertyValue(p);
+        if (value == null) {
+            return null;
+        }
+        return Integer.parseInt(value);
+    }
+
+    @Produces
+    @ConfigProperty
+    public Boolean getBooleanProperty(final InjectionPoint p) {
+        final String value = getPropertyValue(p);
+        if (value == null) {
+            return null;
+        }
+        return Boolean.parseBoolean(value);
+    }
+
+    private String getPropertyValue(final InjectionPoint p) {
+        final ConfigProperty configProperty = p.getAnnotated().getAnnotation(ConfigProperty.class);
+        final String propertyKey = configProperty.key();
+        final String propertyBundle = configProperty.bundle();
+
+        if (StringUtils.isEmpty(propertyKey)) {
+            return configProperty.defaultValue();
+        }
+
+        try {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle(propertyBundle);
+            return resourceBundle.getString(propertyKey);
+
+        } catch (MissingResourceException e) {
+            if (configProperty.mandatory()) {
+                throw e;
+            } else {
+                return configProperty.defaultValue();
+            }
+        }
+    }
+}
