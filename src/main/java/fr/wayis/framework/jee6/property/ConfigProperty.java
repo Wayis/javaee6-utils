0dd68df4600b31347e43621422020e0879dc16e4
Luc LABBE <luc.labbe@inge.neo-soft.fr>
2014-03-14 14:50:54 +0100

    Add more JavaDoc.

--- src/main/java/fr/wayis/framework/jee6/property/ConfigProperty.java
+++ src/main/java/fr/wayis/framework/jee6/property/ConfigProperty.java
@@ -8,22 +8,41 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Qualifier for Configuration properties.
+ * Qualifier for Configuration properties.<br/>
+ * The key is searched into the bundle file. Default bundle is 'config.*'.<br/>
+ * If the property is declared mandatory and is not found in the bundle, a {@link java.util.MissingResourceException} is thrown during the deployment.<br/>
+ * If the key is missing or not found in the bundle file, the default value is returned.
  */
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 public @interface ConfigProperty {
 
+    /**
+     * The bundle where is declared the property.<br/>
+     * Default value: 'config'.
+     */
     @Nonbinding
-    public String bundle() default "deploy";
+    public String bundle() default "config";
 
+    /**
+     * The property key.<br/>
+     * Default value: ''.
+     */
     @Nonbinding
     public String key() default "";
 
+    /**
+     * Is the property mandatory ?<br/>
+     * Default value: false.
+     */
     @Nonbinding
     public boolean mandatory() default false;
 
+    /**
+     * The default value to use if no property key found.<br/>
+     * Default value: ''.
+     */
     @Nonbinding
     public String defaultValue() default "";
 }
--- src/main/java/fr/wayis/framework/jee6/property/ConfigPropertyProducer.java
+++ src/main/java/fr/wayis/framework/jee6/property/ConfigPropertyProducer.java
@@ -2,44 +2,85 @@ package fr.wayis.framework.jee6.property;
 
 import org.apache.commons.lang3.StringUtils;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
- * Producer associated to the {@link ConfigProperty} qualifier.
+ * Producer associated to the {@link fr.wayis.framework.jee6.property.ConfigProperty} qualifier.<br/>
+ * It manages:
+ * <ul>
+ * <li>String</li>
+ * <li>Integer</li>
+ * <li>Boolean</li>
+ * </ul>
+ * <p/>
+ * It searches the property key in the declared bundle.
  */
+@ApplicationScoped
 public class ConfigPropertyProducer {
 
+    /**
+     * Producer for the {@link fr.wayis.framework.jee6.property.ConfigProperty} qualifer for String value.
+     *
+     * @param injectionPoint The injection point.
+     * @return The String value corresponding to the key from the declared bundle.
+     */
     @Produces
+    @Dependent
     @ConfigProperty
-    public String getStringProperty(final InjectionPoint p) {
-        return getPropertyValue(p);
+    public String getStringProperty(final InjectionPoint injectionPoint) {
+        return getPropertyValue(injectionPoint);
     }
 
+    /**
+     * Producer for the {@link fr.wayis.framework.jee6.property.ConfigProperty} qualifer for Integer value.
+     *
+     * @param injectionPoint The injection point.
+     * @return The Integer value corresponding to the key from the declared bundle.
+     */
     @Produces
+    @Dependent
     @ConfigProperty
-    public Integer getIntegerProperty(final InjectionPoint p) {
-        final String value = getPropertyValue(p);
+    public Integer getIntegerProperty(final InjectionPoint injectionPoint) {
+        final String value = getPropertyValue(injectionPoint);
         if (value == null) {
             return null;
         }
         return Integer.parseInt(value);
     }
 
+    /**
+     * Producer for the {@link fr.wayis.framework.jee6.property.ConfigProperty} qualifer for Boolean value.
+     *
+     * @param injectionPoint The injection point.
+     * @return The Boolean value corresponding to the key from the declared bundle.
+     */
     @Produces
+    @Dependent
     @ConfigProperty
-    public Boolean getBooleanProperty(final InjectionPoint p) {
-        final String value = getPropertyValue(p);
+    public Boolean getBooleanProperty(final InjectionPoint injectionPoint) {
+        final String value = getPropertyValue(injectionPoint);
         if (value == null) {
             return null;
         }
         return Boolean.parseBoolean(value);
     }
 
-    private String getPropertyValue(final InjectionPoint p) {
-        final ConfigProperty configProperty = p.getAnnotated().getAnnotation(ConfigProperty.class);
+    /**
+     * Gets a property value.<br/>
+     * Retrieves the {@link fr.wayis.framework.jee6.property.ConfigProperty} annotation from the injection point.<br/>
+     * Searches the key in the declared bundle. The default bundle is a 'config.*' file.
+     *
+     * @param injectionPoint The injection point.
+     * @return The default value if the key is missing or the key is not found in the bundle. Otherwise the found property value.
+     * @throws java.util.MissingResourceException If the property is declared mandatory and is not found in the bundle.
+     */
+    private String getPropertyValue(final InjectionPoint injectionPoint) throws MissingResourceException {
+        final ConfigProperty configProperty = injectionPoint.getAnnotated().getAnnotation(ConfigProperty.class);
         final String propertyKey = configProperty.key();
         final String propertyBundle = configProperty.bundle();
 
